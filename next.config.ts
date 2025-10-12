93c77f55ee0cfbbdaff59f5d12432736d7c0d3f0
Rich Hanes <georgebastille@gmail.com>
2025-10-12 20:30:43 +0100

    Add files to vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    outputFileTracingIncludes: {
+      "/api/stations": ["./src/data/**/*.jsonl"],
+      "/api/fairest": ["./src/data/**/*.jsonl"],
+    },
+  },
 };
 
 export default nextConfig;
